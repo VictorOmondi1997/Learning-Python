cf9f569f284b2b9bb945390107126268db0f1e54
VictorOmondi1997 <info.victoromondi@gmail.com>
2019-02-27 21:40:33 +0300

    added app.py config.py new.py

---
+++ flask/config.py
@@ -0,0 +1,10 @@
+# Config.py
+
+import os
+
+class Config(object):
+	#setting debugging on
+	DEBUG = True
+	# connect ot database
+	SQLALCHEMY_DATABASE_URL = "mysql://VictorOmondi1997:Vic0714452694/db"
+	SECRET_KEY = "Thisissecret"
